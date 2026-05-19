925f89869095304e65d866c42be5ba522a9dd8b2
Diego Guerra <a00835430@tec.mx>
2026-05-19 17:09:48 -0600

    fix/sql import
    fixed an error in a new sql

--- scripts/InsertZonaHorarias.sql
+++ scripts/InsertZonaHorarias.sql
@@ -7,4 +7,4 @@ INSERT INTO zona_horaria (nombre) VALUES
   -- United States
   ('America/New_York'),     -- Eastern — UTC-5 / UTC-4
   ('America/Chicago'),      -- Central — UTC-6 / UTC-5
-  ('America/Los_Angeles'),  -- Pacific — UTC-8 / UTC-7
+  ('America/Los_Angeles');  -- Pacific — UTC-8 / UTC-7
